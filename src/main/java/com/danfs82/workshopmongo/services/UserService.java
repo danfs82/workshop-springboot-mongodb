5cda8d82737b63c9c5f936ff25522616670e53f0
Daniel Souza <daniel.f.souza@hotmail.com>
2019-07-03 00:40:03 -0300

    User by ID

--- src/main/java/com/danfs82/workshopmongo/services/UserService.java
+++ src/main/java/com/danfs82/workshopmongo/services/UserService.java
@@ -1,23 +1,30 @@
 package com.danfs82.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.danfs82.workshopmongo.domain.User;
 import com.danfs82.workshopmongo.repository.UserRepository;
+import com.danfs82.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	
-	public List<User> findAll () {
-		
+
+	public List<User> findAll() {
+
 		return repo.findAll();
+
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		
 	}
 
---
+++ src/main/java/com/danfs82/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,14 @@
+package com.danfs82.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException (String msg) {
+		
+		super(msg);
+	}
+	
+	
+
+}
