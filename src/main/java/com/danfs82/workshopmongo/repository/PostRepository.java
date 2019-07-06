9a1f80dfe97adcc7877d2283b805ba4bc4cb925b
Daniel Souza <daniel.f.souza@hotmail.com>
2019-07-05 23:28:49 -0300

    Consulta com @queries

--- src/main/java/com/danfs82/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/danfs82/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.danfs82.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.danfs82.workshopmongo.domain.Post;
@@ -12,5 +13,8 @@ public interface PostRepository extends MongoRepository<Post, String> {
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
-
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+ 
 }
